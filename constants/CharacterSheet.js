db765d15425dffae6261c8628e8e2c5fe3c78c50
Matthew Jones <matthewarjones@outlook.com>
2021-08-20 17:29:07 +0100

    adding constants

---
+++ constants/CharacterSheet.js
@@ -0,0 +1,556 @@
+export const savingThrows = [
+    {
+        id: "ST Strength",
+        pdfId: "ST Strength",
+        type: "PDFTextField"
+    },
+    {
+        id: "ST Strength Prof",
+        pdfId: "Check Box 11",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "ST Dexterity",
+        pdfId: "ST Dexterity",
+        type: "PDFTextField"
+    },
+    {
+        id: "ST Dexterity Prof",
+        pdfId: "Check Box 18",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "ST Constitution",
+        pdfId: "ST Constitution",
+        type: "PDFTextField"
+    },
+    {
+        id: "ST Constitution Prof",
+        pdfId: "Check Box 19",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "ST Intelligence",
+        pdfId: "ST Intelligence",
+        type: "PDFTextField"
+    },
+    {
+        id: "ST Intelligence Prof",
+        pdfId: "Check Box 20",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "ST Wisdom",
+        pdfId: "ST Wisdom",
+        type: "PDFTextField"
+    },
+    {
+        id: "ST Wisdom Prof",
+        pdfId: "Check Box 21",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "ST Charisma",
+        pdfId: "ST Charisma",
+        type: "PDFTextField"
+    },
+    {
+        id: "ST Charisma Prof",
+        pdfId: "Check Box 22",
+        type: "PDFCheckBox"
+    },
+];
+
+export const attributes = [
+    {
+        id: "STR",
+        pdfId: "STR",
+        type: "PDFTextField"
+    },
+    {
+        id: "STR mod",
+        pdfId: "STRmod",
+        type: "PDFTextField"
+    },
+    {
+        id: "DEX",
+        pdfId: "DEX",
+        type: "PDFTextField"
+    },
+    {
+        id: "DEX mod",
+        pdfId: "DEXmod ",
+        type: "PDFTextField"
+    },
+    {
+        id: "CON",
+        pdfId: "CON",
+        type: "PDFTextField"
+    },
+    {
+        id: "CON mod",
+        pdfId: "CONmod",
+        type: "PDFTextField"
+    },
+    {
+        id: "INT",
+        pdfId: "INT",
+        type: "PDFTextField"
+    },
+    {
+        id: "INT mod",
+        pdfId: "INTmod",
+        type: "PDFTextField"
+    },
+    {
+        id: "WIS",
+        pdfId: "WIS",
+        type: "PDFTextField"
+    },
+    {
+        id: "WIS mod",
+        pdfId: "WISmod",
+        type: "PDFTextField"
+    },
+    {
+        id: "CHA",
+        pdfId: "CHA",
+        type: "PDFTextField"
+    },
+    {
+        id: "CHA mod",
+        pdfId: "CHamod",
+        type: "PDFTextField"
+    },
+]
+
+export const weapons = [
+    {
+        id: "Weapon 1 Name",
+        pdfId: "Wpn Name",
+        type: "PDFTextField"
+    },
+    {
+        id: "Weapon 1 Attack Bonus",
+        pdfId: "Wpn1 AtkBonus",
+        type: "PDFTextField"
+    },
+    {
+        id: "Weapon 1 Damage",
+        pdfId: "Wpn1 Damage",
+        type: "PDFTextField"
+    },
+    {
+        id: "Weapon 2 Name",
+        pdfId: "Wpn Name 2",
+        type: "PDFTextField"
+    },
+    {
+        id: "Weapon 2 Attack Bonus",
+        pdfId: "Wpn2 AtkBonus ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Weapon 2 Damage",
+        pdfId: "Wpn2 Damage ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Weapon 3 Name",
+        pdfId: "Wpn Name 3",
+        type: "PDFTextField"
+    },
+    {
+        id: "Weapon 3 Attack Bonus",
+        pdfId: "Wpn3 AtkBonus  ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Weapon 3 Damage",
+        pdfId: "Wpn3 Damage ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Attacks and Spellcasting",
+        pdfId: "AttacksSpellcasting",
+        type: "PDFTextField"
+    }
+]
+
+export const skills = [
+    {
+        id: "Acrobatics",
+        pdfId: "Acrobatics",
+        type: "PDFTextField"
+    },
+    {
+        id: "Acrobatics Prof",
+        pdfId: "Check Box 23",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Animal Handling",
+        pdfId: "Animal",
+        type: "PDFTextField"
+    },
+    {
+        id: "Animal Handling Prof",
+        pdfId: "Check Box 24",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Arcana",
+        pdfId: "Arcana",
+        type: "PDFTextField"
+    },
+    {
+        id: "Arcana Prof",
+        pdfId: "Check Box 25",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Athletics",
+        pdfId: "Athletics",
+        type: "PDFTextField"
+    },
+    {
+        id: "Athletics Prof",
+        pdfId: "Check Box 26",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Deception",
+        pdfId: "Deception ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Deception Prof",
+        pdfId: "Check Box 27",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "History",
+        pdfId: "History ",
+        type: "PDFTextField"
+    },
+    {
+        id: "History Prof",
+        pdfId: "Check Box 28",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Insight",
+        pdfId: "Insight",
+        type: "PDFTextField"
+    },
+    {
+        id: "Insight Prof",
+        pdfId: "Check Box 29",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Intimidation",
+        pdfId: "Intimidation",
+        type: "PDFTextField"
+    },
+    {
+        id: "Intimidation Prof",
+        pdfId: "Check Box 30",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Investigation",
+        pdfId: "Investigation ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Investigation Prof",
+        pdfId: "Check Box 31",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Medicine",
+        pdfId: "Medicine",
+        type: "PDFTextField"
+    },
+    {
+        id: "Medicine Prof",
+        pdfId: "Check Box 32",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Nature",
+        pdfId: "Nature",
+        type: "PDFTextField"
+    },
+    {
+        id: "Nature Prof",
+        pdfId: "Check Box 33",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Perception",
+        pdfId: "Perception ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Perception Prof",
+        pdfId: "Check Box 34",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Performance",
+        pdfId: "Performance",
+        type: "PDFTextField"
+    },
+    {
+        id: "Performance Prof",
+        pdfId: "Check Box 35",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Persuasion",
+        pdfId: "Persuasion",
+        type: "PDFTextField"
+    },
+    {
+        id: "Persuasion Prof",
+        pdfId: "Check Box 36",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Religion",
+        pdfId: "Religion",
+        type: "PDFTextField"
+    },
+    {
+        id: "Religion Prof",
+        pdfId: "Check Box 37",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Sleight of Hand",
+        pdfId: "SleightofHand",
+        type: "PDFTextField"
+    },
+    {
+        id: "Sleight of Hand Prof",
+        pdfId: "Check Box 38",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Stealth",
+        pdfId: "Stealth ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Stealth Prof",
+        pdfId: "Check Box 39",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "Survival",
+        pdfId: "Survival",
+        type: "PDFTextField"
+    },
+    {
+        id: "Survival Prof",
+        pdfId: "Check Box 40",
+        type: "PDFCheckBox"
+    }
+]
+
+export const header = [
+    {
+        id: "Level",
+        pdfId: "ClassLevel",
+        type: "PDFTextField"
+    },
+    {
+        id: "Background",
+        pdfId: "Background",
+        type: "PDFTextField"
+    },
+    {
+        id: "Player Name",
+        pdfId: "PlayerName",
+        type: "PDFTextField"
+    },
+    {
+        id: "Character Name",
+        pdfId: "CharacterName",
+        type: "PDFTextField"
+    },
+    {
+        id: "Race",
+        pdfId: "Race ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Alignment",
+        pdfId: "Alignment",
+        type: "PDFTextField"
+    },
+    {
+        id: "XP",
+        pdfId: "XP",
+        type: "PDFTextField"
+    }
+]
+
+export const characterStats = [
+    {
+        id: "AC",
+        pdfId: "AC",
+        type: "PDFTextField"
+    },
+    {
+        id: "Initiative",
+        pdfId: "Initiative",
+        type: "PDFTextField"
+    },
+    {
+        id: "Speed",
+        pdfId: "Speed",
+        type: "PDFTextField"
+    },
+    {
+        id: "HP Max",
+        pdfId: "HPMax",
+        type: "PDFTextField"
+    },
+    {
+        id: "HP Current",
+        pdfId: "HPCurrent",
+        type: "PDFTextField"
+    },
+    {
+        id: "HP Temp",
+        pdfId: "HPTemp",
+        type: "PDFTextField"
+    },
+    {
+        id: "HD Total",
+        pdfId: "HDTotal",
+        type: "PDFTextField"
+    },
+    {
+        id: "Hit Dice",
+        pdfId: "HD",
+        type: "PDFTextField"
+    },
+    {
+        id: "DS success 1",
+        pdfId: "Check Box 12",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "DS success 2",
+        pdfId: "Check Box 13",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "DS success 3",
+        pdfId: "Check Box 14",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "DS fail 1",
+        pdfId: "Check Box 15",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "DS fail 2",
+        pdfId: "Check Box 16",
+        type: "PDFCheckBox"
+    },
+    {
+        id: "DS fail 3",
+        pdfId: "Check Box 17",
+        type: "PDFCheckBox"
+    }
+]
+
+export const backstory = [
+    {
+        id: "Ideals",
+        pdfId: "Ideals",
+        type: "PDFTextField"
+    },
+    {
+        id: "Bonds",
+        pdfId: "Bonds",
+        type: "PDFTextField"
+    },
+    {
+        id: "Personality Traits",
+        pdfId: "PersonalityTraits ",
+        type: "PDFTextField"
+    },
+    {
+        id: "Flaws",
+        pdfId: "Flaws",
+        type: "PDFTextField"
+    }
+]
+
+export const money = [
+    {
+        id: "Copper",
+        pdfId: "CP",
+        type: "PDFTextField"
+    },
+    {
+        id: "Silver",
+        pdfId: "SP",
+        type: "PDFTextField"
+    },
+    {
+        id: "Electrum",
+        pdfId: "EP",
+        type: "PDFTextField"
+    },
+    {
+        id: "Gold",
+        pdfId: "GP",
+        type: "PDFTextField"
+    },
+    {
+        id: "Platinum",
+        pdfId: "PP",
+        type: "PDFTextField"
+    },
+]
+
+export const fields = [
+    {
+        id: "Inspiration",
+        pdfId: "Inspiration",
+        type: "PDFTextField"
+    },
+    {
+        id: "Prof Bonus",
+        pdfId: "ProfBonus",
+        type: "PDFTextField"
+    },
+    {
+        id: "Passive Perception",
+        pdfId: "Passive",
+        type: "PDFTextField"
+    },
+    {
+        id: "Other Proficiencies and Languages",
+        pdfId: "ProficienciesLang",
+        type: "PDFTextField"
+    }
+].concat(
+    header,
+    backstory,
+    characterStats,
+    savingThrows,
+    attributes,
+    skills,
+    money,
+    weapons);
+
+export default { fields };
---
+++ constants/Form.js
@@ -0,0 +1,9 @@
+export const formTypes = {
+    TEXT: "PDFTextField",
+    CHECKBOX: "PDFCheckBox"
+}
+
+
+
+
+export default { formTypes };
