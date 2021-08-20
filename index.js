5beb2956a7358194765550510ebcd356a273f761
Matthew Jones <matthewarjones@outlook.com>
2021-08-20 17:27:51 +0100

    WIP - started to fill in the character sheet

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 import { PDFDocument } from "pdf-lib";
-import {readFile} from "fs";
- 
+import {readFile, writeFile} from "fs";
+import {fields} from './constants/CharacterSheet.js';
+import {formTypes} from './constants/Form.js';
 const sourcePDFPath = "test/5E_CharacterSheet_Fillable.pdf";
-var sourcePDF;
-readFile(sourcePDFPath, (err, data) =>{
+
+readFile(sourcePDFPath, async (err, data) => {
     if (err) throw err;
-    sourcePDF = data;
-    const pdfDoc = PDFDocument.load(sourcePDF).then(data => {
-        data.getForm().getFields().forEach(field => {
-            const type = field.constructor.name
-            const name = field.getName()
-            console.log(`${type}: ${name}`)
-          })
-    });
+    const pdfDoc = await PDFDocument.load(data);
+    const form = pdfDoc.getForm();
+    
+    populateForm(form);
+    
+    const pdfBytes = await pdfDoc.save();
+    writeFileToLocal(pdfBytes);
+    // listAllFieldsInForm(form)
 });
 
- 
+function writeFileToLocal(pdfBytes) {
+    writeFile("test.pdf", pdfBytes, function (err) {
+        if (err) return console.log(err);
+        console.log('File has been written');
+    });
+}
+
+function listAllFieldsInForm(form){
+    //DEBUG
+    form.getFields().forEach(id => {
+        const type = id.constructor.name;
+        const name = id.getName();
+        // console.log(id)
+        
+        console.log(`${type}: ${name}`)
+    })
+}
+
+function populateForm(form) {
+    for( var i in fields) {
+        switch(fields[i].type){
+            case formTypes.TEXT: {
+                const textField = form.getTextField(fields[i].pdfId);
+                textField.setText("WOOO");
+                break;
+            }
+            case formTypes.CHECKBOX: {
+                const checkbox = form.getCheckBox(fields[i].pdfId);
+                checkbox.check();
+                break;
+            }
+        }
+        
+    }
+}
